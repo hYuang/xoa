c217ed4ccf1740929d70d1fcb5702373749c7489
hYuang <familyhuangyang@hotmail.com>
2020-04-30 22:37:09 +0800

    init project

---
+++ src/main/java/com/xoa/XoaApplication.java
@@ -0,0 +1,13 @@
+package com.xoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xoa/XoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
